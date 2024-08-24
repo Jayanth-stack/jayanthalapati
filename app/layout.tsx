7dfe548c3c2203d07c21ed8781d2a3b600b4a303
Jayant-stack <jayanthalapati@gmail.com>
2024-08-24 17:46:38 -0400

    adding my experiences

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jayanth's Portifolio",
-  description: "Modern Web Based Portifolio",
+  description: "Modern Next.js Web Based Portifolio",
 };
 
 export default function RootLayout({
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+        </ThemeProvider>
+        
+      </body>
     </html>
   );
 }
