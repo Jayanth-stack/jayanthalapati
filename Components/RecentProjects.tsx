18b717bf4747cc41f154be7934f0084df06d57de
Jayant-stack <jayanthalapati@gmail.com>
2024-09-18 16:28:45 -0400

    checked run time issues

--- Components/RecentProjects.tsx
+++ Components/RecentProjects.tsx
@@ -1,16 +1,14 @@
-import { projects } from '@/data'
-import React from 'react'
-import { div } from 'three/webgpu'
-import { PinContainer } from './ui/3d-pin'
-import { link } from 'fs'
-import { FaLocationArrow } from 'react-icons/fa'
+"use client";
+import { FaLocationArrow } from "react-icons/fa6";
+import { projects } from "@/data";
+import { PinContainer } from "@/Components/ui/3d-pin";
 
 const RecentProjects = () => {
   return (
     <div className="py-20">
-      <h1 className= "text text-5xl">
-        A small selection of {" "}
-        <span className="text-purple"> recent projects</span>
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map((item) => (
@@ -18,8 +16,7 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <PinContainer
-            >
+            <PinContainer>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
@@ -65,7 +62,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check my Work
+                    Check My live work
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
