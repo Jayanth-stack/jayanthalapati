bef831bebd2148487ba37f261333f64d963cb3d5
Jayanth-stack <jayanthalapati@gmail.com>
2025-05-19 22:10:37 -0400

    Configure Next.js for static export and set base path; add export script to package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/jayanthalapati' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
